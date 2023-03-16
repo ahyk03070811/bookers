c225b91ecedd313c36818358b1bba462eb0a49a1
ahyk03070811 <126743881+ahyk03070811@users.noreply.github.com>
2023-03-16 07:02:33 +0000

    3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to book_path
+    @book=Book.new(book_params)
+    if @book.save
+      flash[:notice]="successfully"
+      redirect_to book_path
+    else
+      @books=Book.all
+      render :index
+    end
   end
 
   def show
@@ -18,6 +23,17 @@ class BooksController < ApplicationController
     @book=Book.find(params[:id])
   end
 
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
 
  private
 
